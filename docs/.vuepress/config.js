6b0c295c6cecb0c7fbb65f05965ab48fba57063b
scammo <sbrinkmann@54gradsoftware.de>
2025-08-14 17:09:05 +0200

    image path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,7 @@ export default defineUserConfig({
   description: 'Der Smarte Gemeinschaftskalender',
 
   theme: defaultTheme({
-    logo: 'DSG_Logo_Dokumentation.png',
+    logo: 'DSG-Logo-Dokumentation.png',
 
     navbar: [
   { text: 'Handbuch', link: '/get-started' },
